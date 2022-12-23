c85593d95031197604bbe5263e361b3125e344f2
tanaka <21c1080011ta@edu.tech.ac.jp>
2022-12-23 15:38:03 +0900

    audio_file to text

--- att.py
+++ att.py
@@ -3,7 +3,9 @@ import time
 
 # Creates an instance of a speech config with specified subscription key and service region.
 # Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region, language = "3b12ccebd5ba4fadbdf187bbe8de9289", "eastasia", "ja-JP"
+
+# SPEECH_KEY, SPEECH_REGION には setx SPEECH_KEY your-key をコンソールで実行する必要がある
+speech_key, service_region, language = "os.environ.get('SPEECH_KEY')", "os.environ.get('SPEECH_REGION')", "ja-JP"
 speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
 
 # to text files
@@ -12,15 +14,12 @@ while True:
     try:
         path_w = "text_data/from_audio/" + str(num) + ".txt"
         f = open(path_w, mode='x')
-        #print("書き込めた")
         break
     except:
         num += 1
-        #print("書き込めなかった")
-#print("抜けた")
 
 # from audio file
-audio_filename = "audio_data/sampledata_audiofiles_aboutSpeechSdk.wav"
+audio_filename = "audio_data/your_audiofile.wav"
 audio_input = speechsdk.audio.AudioConfig(filename=audio_filename)
 
 # Creates a recognizer with the given settings
